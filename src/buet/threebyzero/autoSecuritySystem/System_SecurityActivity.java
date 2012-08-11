598e930e82deb2a15edb7b0e0a920aa5dd9bfad6
user.faysal <faysalhossain2007@gmail.com>
2012-08-12 00:54:13 +0600

    START

---
+++ src/buet/threebyzero/autoSecuritySystem/System_SecurityActivity.java
@@ -0,0 +1,13 @@
+package buet.threebyzero.autoSecuritySystem;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class System_SecurityActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
